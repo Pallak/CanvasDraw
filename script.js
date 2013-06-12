40435c7eeca43723b920377f2402e881e4ffadf2
Pallak Grewal <pallak.grewal@gmail.com>
2013-06-12 02:58:53 -0400

    Added functionality to display selected menu on top of the page.
    Changed all HTML text to lowercase and used capitalize property.

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
 
 	/*******************************
-	Highlighting toolbar items
+	Adding selected menu on the top.
+	Hides all the menus and then 
+	shows the selected one
+	*******************************/
+	function showSelectedMenu(item){
+		$(".menu div").each(function() {
+			$(this).hide();
+		});
+		var menuName = item.text();
+		$("."+menuName).show();
+	}
+	/*******************************/
+
+	/*******************************
+	Highlighting toolbar items.
+	Removes highlight from all menu 
+	items and then highlights the 
+	selected item
 	*******************************/
 	function selectItem (item) {
-		$(".toolbar li").each(function() {
-			$(this).removeClass("highlightTool");
-		});
-		item.addClass("highlightTool");
+		if(item.index()!=5){
+			$(".toolbar li").each(function() {
+				$(this).removeClass("highlightTool");
+			});
+			item.addClass("highlightTool");
+			showSelectedMenu(item);
+		}
+		/* if item is "clear canvas" treat it 
+		as a button*/
+		else{
+
+		}
 	}
 
 	selectItem($(".toolbar li").first());
